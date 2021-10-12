7c2e411e95e66920863309a13e1071e95b321805
zm20304 <zm20304@josai.ac.jp>
2021-10-12 04:52:07 +0000

    111

--- Myjs.js
+++ Myjs.js
@@ -1,4 +1,4 @@
-alert('Hello, saito!'); alert(1+1);alert(Math.sqrt(1+1));
+alert('Hello, saitou!'); alert(1+1);alert(Math.sqrt(1+1));
 alert(2+3);alert(Math.sqrt(2+3));
 alert('2'+'3');alert(Math.sqrt('2'+'3'));
 alert(Number('2')+Number('3'));alert(Math.sqrt(Number('2')+Number('3')));
