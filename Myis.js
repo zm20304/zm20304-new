90949db4cd587a73083ea2b85207469f48ee6bd7
zm20304 <zm20304@josai.ac.jp>
2021-10-12 04:17:41 +0000

    111

---
+++ Myis.js
@@ -0,0 +1,6 @@
+alert('Hello, Saitou!'); alert(1+1);alert(Math.sqrt(1+1));
+alert(2+3);alert(Math.sqrt(2+3));
+alert('2'+'3');alert(Math.sqrt('2'+'3'));
+alert(Number('2')+Number('3'));alert(Math.sqrt(Number('2')+Number('3')));
+alert('2'+3);alert(Math.sqrt('2'+3));
+alert(Number('2')+3);alert(Math.sqrt(Number('2')+3));
