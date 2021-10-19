e64904105b097faab23214fcfc232f6d1cf79f08
zm20304 <zm20304@josai.ac.jp>
2021-10-19 01:12:58 +0000

    311

--- Myjs.js
+++ Myjs.js
@@ -1,4 +1,6 @@
-// alert('Hello, Osamu!');// alert(1+1);alert(Math.sqrt(1+1));
+ alert('Hello, Saitou!');
+ alert(1+1);
+alert(Math.sqrt(1+1));
 alert(2+3);alert(Math.sqrt(2+3));
 alert('2'+'3');alert(Math.sqrt('2'+'3'));
 alert(Number('2')+Number('3'));alert(Math.sqrt(Number('2')+Number('3')));
