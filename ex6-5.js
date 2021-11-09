4a5ec1be44a9b230db0e9b934c8cf8dbc30c7293
zm20304 <zm20304@josai.ac.jp>
2021-11-09 01:40:36 +0000

    ３１１

--- ex6-5.js
+++ ex6-5.js
@@ -3,3 +3,6 @@ var who65 = 'world';function sayhello65()
 function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 function gakita(){alert( who65 + 'が来た!');}
+function rei65() {who65 = 'レイ';}
+function shinnji65() {who65 = 'シンジ';}
+function asuka65() {who65 = 'アスカ';}
