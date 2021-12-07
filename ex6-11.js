311d150e895fcdffa3e5913e7acd20ee3a1c9d6e
zm20304 <zm20304@josai.ac.jp>
2021-12-07 04:03:33 +0000

    311

---
+++ ex6-11.js
@@ -0,0 +1,7 @@
+function hyouji(){
+     //表示する文字
+     varstr= "一文字ずつ表示します。"; 
+     //テキストボックスの文字数
+     varcnt= document.timer.moji.value.length;
+     //文字が全部表示されているか確認if ( cnt< 11 ) { 
+        //現在より1文字多く切り出して表示
