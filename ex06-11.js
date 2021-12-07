679122c7a16e57988381a83655f1389ce94e8610
zm20304 <zm20304@josai.ac.jp>
2021-12-07 06:21:09 +0000

    ３２１

---
+++ ex06-11.js
@@ -0,0 +1,15 @@
+function hyouji(一文字ずつ){
+ //表示する文字
+ varstr= "一文字ずつ表示します";
+ //テキストボックスの文字数
+ var cnt= document.timer.moji.value.length; 
+ //文字が全部表示されているか確認
+ if ( cnt< 11 ) 
+   //現在より1文字多く切り出して表示
+   document.timer.moji.value= str.substr(0,cnt+1); } else{ 
+       //全て表示されたら、空文字に戻す
+       document.timer.moji.value= "一文字ずつ"; } 
+       function startfnc(一文字ずつ表示します) { 
+           //関数hyoji()を1000ミリ秒間隔で呼び出す
+           setInterval("hyoji(一文字ずつ)",1000); } 
+           function mes(一文字ずつ表示します) { alert("3秒経ちました！"); }  
