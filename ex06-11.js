9bdbaff42dcf90a0b6d2965748b341801f6fe494
zm20304 <zm20304@josai.ac.jp>
2022-02-03 08:40:05 +0000

    311

--- ex06-11.js
+++ ex06-11.js
@@ -13,3 +13,4 @@ var cnt= document.timer.moji.value.length;
      function startfnc() { 
        //関数hyoji()を1000ミリ秒間隔で呼び出す
        setInterval("hyoji()",1000); } 
+       function mes() { alert("3秒経ちました！"); }
