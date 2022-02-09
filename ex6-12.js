34ed9095657fcc67eecf02458d605773da77d6fa
zm20304 <zm20304@josai.ac.jp>
2022-02-09 07:10:06 +0000

    311

--- ex6-12.js
+++ ex6-12.js
@@ -1,15 +1,15 @@
-function hyoji() { 
+function hyoji1() { 
     //表示する文字
     var str= "一文字ずつ表示します。";
      //テキストボックスの文字数
-     var cnt= document.timer.moji.value.length; 
+     var cnt= document.timer1.moji1.value.length; 
      //文字が全部表示されているか確認
      if ( cnt< 11 ) { 
          //現在より1文字多く切り出して表示
-        document.timer.moji.value= str.substr(0,cnt+1); } 
+        document.timer1.moji1.value= str.substr(0,cnt+1); } 
         else { 
             //全て表示されたら、空文字に戻す
-            document.timer.moji.value= ""; } 
+            document.timer1.moji1.value= ""; } 
             //setTimeout()を含む関数を呼び出す
             setTimeout("hyoji()",1000); }
 
